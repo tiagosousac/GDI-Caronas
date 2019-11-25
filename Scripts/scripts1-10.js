839b2075d87f80533c08f2641d478271bed99304
Tiago <tsc2@cin.ufpe.br>
2019-11-25 14:55:44 -0300

    realizando alguns commits, nome do arquivo sujeito a mudancas

--- Scripts/scripts1-10.js
+++ Scripts/scripts1-10.js
@@ -2,3 +2,14 @@ use caronas;
 
 // Conta quantos motoristas possuem apenas um carro
 db.caronas.find({cars:{$size: 1}}).count();
+
+// Mostra, com o comando pretty(), apenas os motoristas que possuem carros.
+db.caronas.find({cars:{$exists:true}}).pretty()
+
+// Mostra a média de nota dos motoristas homens e mulheres
+db.caronas.aggregate([
+  { $match: { cars: {$exists:true} } },
+  { $group: { _id: "$sex", total: { $avg: "$rating" } } },
+  { $sort: { total: -1 } }
+])
+
